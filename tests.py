01ce6798714e7d8fc02e459906899c433d205439
Colin Cheng <chenlinl@oregonstate.edu>
2024-07-27 15:22:02 -0700

    Write test1:test_epoch_start to verify the epoch start is correctly identitfied

--- tests.py
+++ tests.py
@@ -1,10 +1,21 @@
 import unittest
+from task import my_datetime
 
 
 class TestCase(unittest.TestCase):
 
     def test1(self):
         self.assertTrue(True)
+
+
+class TestMyDateTime(unittest.TestCase):
+    """Test the function my_datetime import from task.py"""
+    def test_epoch_start(self):
+        """Test the epoch start is correctly identified."""
+        expected_date = '01-01-1970'
+        result = my_datetime(0)
+        self.assertEqual(result, expected_date)
+
 
 
 if __name__ == '__main__':
