023d36272da632f3b388128d492aebbcfeab57f3
Colin Cheng <chenlinl@oregonstate.edu>
2024-08-02 11:28:22 -0700

    Wrote test3: test_far_future_date to check far future date

--- tests.py
+++ tests.py
@@ -2,12 +2,6 @@ import unittest
 from task import my_datetime
 
 
-class TestCase(unittest.TestCase):
-
-    def test1(self):
-        self.assertTrue(True)
-
-
 class TestMyDateTime(unittest.TestCase):
     """Test the function my_datetime import from task.py"""
     def test_epoch_start(self):
@@ -17,6 +11,10 @@ class TestMyDateTime(unittest.TestCase):
     def test_arbitrary_date(self):
         """Test the example 123456789 whether convert to correct year"""
         self.assertEqual(my_datetime(123456789), '11-29-1973')
+
+    def test_far_future_date(self):
+        """Test the far future date 9876543210 whether convert correctly"""
+        self.assertEqual(my_datetime(9876543210), '12-22-2282')
 
 
 if __name__ == '__main__':
