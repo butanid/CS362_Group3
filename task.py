1a817926c35dd4777ae1ffb7e1c0e1bae1df7d52
Colin Cheng <chenlinl@oregonstate.edu>
2024-08-02 11:33:53 -0700

    Updated the function to convert the leap yearcorrectly, it passed

--- task.py
+++ task.py
@@ -1,19 +1,19 @@
 def my_datetime(num_sec):
     """Function 2: takes num_sec and converts it to a date and returns it as a
     string with the following format MM-DD-YYYY."""
-    # Constants for second, minutes, hours
+    # Constants for second, minutes, hours.
     seconds_per_minute = 60
     minutes_per_hour = 60
     hours_per_day = 24
     seconds_per_day = seconds_per_minute * minutes_per_hour * hours_per_day
 
-    # Leap year check, such as 2000 or 2400 is leap year
+    # Leap year check, such as 2000 or 2400 is leap year.
     def is_leap_year(year):
         if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
             return True
         return False
 
-    # Calculate total days since epoch "01-01-1970"
+    # Calculate total days since epoch "01-01-1970".
     total_days = num_sec // seconds_per_day
 
     # Start from January 1, 1970
@@ -25,7 +25,7 @@ def my_datetime(num_sec):
         total_days -= current_year_days
         year += 1
 
-    # Determine the month and day in the final year
+    # Determine the month and day in the final year.
     month_lengths = [31, 28 + is_leap_year(year), 31, 30, 31, 30, 31, 31,
                      30, 31, 30, 31]
     month = 0
