683844f7dd4548561c5b7da936a7b2783e085f3a
Ibrahim Velasquez <vibrahim09@gmail.com>
2024-08-03 12:42:58 -0700

    removed trailing whitespaces

--- tests.py
+++ tests.py
@@ -5,17 +5,17 @@ from task import conv_num
 class TestConvNum(unittest.TestCase):
 
     def test_ints(self):
-        '''Test if a string containing ints 
+        '''Test if a string containing ints
         returns a int type'''
         self.assertEqual(conv_num("1234560"), 1234560)
 
     def test_float(self):
-        """Test if a string containing floats 
+        """Test if a string containing floats
         returns a float type"""
         self.assertEqual(conv_num("12345.89"), 12345.89)
 
     def test_negative(self):
-        """Test if a string containing negative float 
+        """Test if a string containing negative float
         returns a negative float"""
         self.assertEqual(conv_num("-123.45"), -123.45)
 
