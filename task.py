47dd12b5a9f06ff7fea1cabd6280d7007bd165c9
Ibrahim Velasquez <vibrahim09@gmail.com>
2024-08-03 12:40:11 -0700

    Started implementation for conv_num. Ints pass, floats pass, and negatives pass. Working on Hex numbers and None types.

--- task.py
+++ task.py
@@ -1,2 +1,19 @@
-def my_func():
-    return "Hello World"
+def conv_num(num_str: str) -> float | None:
+    converted_num: int | float = 0
+    negative = False
+    floating_point = False
+    for char in num_str:
+        if char == '-':
+            negative = True
+        elif char == '.':
+            floating_point = True
+        else:
+            converted_num *= 10
+            converted_num += (ord(char) - ord('0'))
+
+    if negative:
+        converted_num = -converted_num
+    if floating_point:
+        converted_num /= 100
+
+    return converted_num
--- tests.py
+++ tests.py
@@ -1,10 +1,23 @@
 import unittest
+from task import conv_num
 
 
-class TestCase(unittest.TestCase):
+class TestConvNum(unittest.TestCase):
 
-    def test1(self):
-        self.assertTrue(True)
+    def test_ints(self):
+        '''Test if a string containing ints 
+        returns a int type'''
+        self.assertEqual(conv_num("1234560"), 1234560)
+
+    def test_float(self):
+        """Test if a string containing floats 
+        returns a float type"""
+        self.assertEqual(conv_num("12345.89"), 12345.89)
+
+    def test_negative(self):
+        """Test if a string containing negative float 
+        returns a negative float"""
+        self.assertEqual(conv_num("-123.45"), -123.45)
 
 
 if __name__ == '__main__':
